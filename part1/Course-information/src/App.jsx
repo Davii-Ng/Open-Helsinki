f6b2aa12ab2f4eea29a69da760feee3ffcbd6546
Davii-Ng <nminhd98@gmail.com>
2025-10-31 01:14:26 +0700

    1.3

--- part1/Course-information/src/App.jsx
+++ part1/Course-information/src/App.jsx
@@ -29,6 +29,7 @@ export default App
 */
 
 const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>{props.course}</h1>
@@ -56,12 +57,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name : 'Fundamentals of  React',
+    exercises: 10
+  }
+  const part2 = {
+    name : 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name : 'State of a component',
+    exercises: 14
+  }
 
 
   return (
