9b8144f38d5ff45f0edd1747d87975923a3fbf24
Davii-Ng <Davii-Ng>
2025-11-22 16:57:22 -0500

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,24 +2,35 @@ import { useState } from 'react'
 
 const Button = ({onClick, text})=> <button onClick = {onClick}>{text}</button>
 
+let clicks = 0
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const[all, setAll] = useState(0)
+
+
 
 
   const handleGoodClick = () =>{
     setGood(good + 1)
+    setAll(all + 1)
+    clicks += 1
   }
   
   const handleBadClick = () =>{
     setBad(bad + 1)
+    setAll(all - 1)
+    clicks += 1
   }
   
   const handleNeutralClick = () =>{
     setNeutral(neutral + 1)
+    clicks += 1
   }
+  
 
   return (
     <div>
@@ -31,6 +42,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all/clicks}</p>
+      <p>positive {good/clicks}%</p>
     </div>
   )
 }
