7f606dfa1941b01dd2b2f1b110dba1ea9ef0ada4
Davii-Ng <Davii-Ng>
2025-11-22 17:28:11 -0500

    debug, correcting format

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,50 +1,31 @@
 import { useState } from 'react'
 
-const Button = ({onClick, text})=> <button onClick = {onClick}>{text}</button>
-
-let clicks = 0
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const[all, setAll] = useState(0)
 
 
 
 
-  const handleGoodClick = () =>{
-    setGood(good + 1)
-    setAll(all + 1)
-    clicks += 1
-  }
-  
-  const handleBadClick = () =>{
-    setBad(bad + 1)
-    setAll(all - 1)
-    clicks += 1
-  }
-  
-  const handleNeutralClick = () =>{
-    setNeutral(neutral + 1)
-    clicks += 1
-  }
   
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={handleGoodClick} text = "good"/>
-      <Button onClick={handleNeutralClick} text = "neutral"/>
-      <Button onClick={handleBadClick} text = "bad"/>
+      <button onClick={() => setGood(good+1)}>good</button> 
+      <button onClick={() => setNeutral(neutral+1)}>neutral</button>
+      <button onClick={() => setBad(bad+1)}>bad</button>
 
+      <h1>Statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {all/clicks}</p>
-      <p>positive {good/clicks}%</p>
+      <p>all {good + bad + neutral}</p>
+      <p>average {(good - bad)/ (good + bad + neutral)}</p>
+      <p>positive {good/(good + bad + neutral)}%</p>
     </div>
   )
 }
