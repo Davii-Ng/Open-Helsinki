6933c46f6061389b7c6e7d9c675bc891457e9f0f
Davii-Ng <nminhd98@gmail.com>
2025-11-26 01:50:22 -0500

    2.9

--- part2/The Phonebook/src/App.jsx
+++ part2/The Phonebook/src/App.jsx
@@ -9,10 +9,21 @@ const DisplayPerson = ({person}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filterPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   const CheckDuplicate = () => {
     for (let i = 0; i < persons.length; i++){
@@ -44,11 +55,17 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+
   
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value = {filter} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit = {addName}>
         <div>
           name: <input value = {newName} onChange = {handleNameChange}/>
@@ -61,7 +78,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <DisplayPerson key = {person.name} person = {person}/>)}
+      {filterPersons.map(person => <DisplayPerson key = {person.id} person = {person}/>)}
     </div>
   )
 }
